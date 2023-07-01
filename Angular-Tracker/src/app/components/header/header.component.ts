6301123bc2e76959bdfc26cf163515e28027fff8
Lordprettyflacko <snsolomar@gmail.com>
2023-06-30 18:19:59 -0800

    toggle the add button click

--- Angular-Tracker/src/app/components/header/header.component.ts
+++ Angular-Tracker/src/app/components/header/header.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  
+
   constructor() {}
 
   ngOnInit(): void {
     
+  }
+
+  toggleAddTask() {
+    console.log('toggle');
   }
 
 }
