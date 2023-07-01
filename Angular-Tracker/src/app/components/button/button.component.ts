c6be64677dc8fc9b2ec35014b5acafad1164fee9
Lordprettyflacko <snsolomar@gmail.com>
2023-06-30 17:45:02 -0800

    first button

--- Angular-Tracker/src/app/components/button/button.component.ts
+++ Angular-Tracker/src/app/components/button/button.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
