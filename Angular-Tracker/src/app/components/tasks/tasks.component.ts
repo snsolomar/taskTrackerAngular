1fff3a200dab2cc0bd180ec92c948d7982e549c8
Simon Solomon <snsolomar@gmail.com>
2023-07-02 15:15:46 -0500

    delete task

--- Angular-Tracker/src/app/components/tasks/tasks.component.ts
+++ Angular-Tracker/src/app/components/tasks/tasks.component.ts
@@ -20,4 +20,13 @@ export class TasksComponent implements OnInit {
     // think of subscribe as a promise
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   };
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
 }
--- Angular-Tracker/src/app/services/task.service.ts
+++ Angular-Tracker/src/app/services/task.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { TASKS } from '../mock.tasks';
 // Interface
 import { Task } from 'src/app/Task';
-import { Observable, of } from 'rxjs'; 
+import { Observable } from 'rxjs'; 
 
 @Injectable({
   providedIn: 'root'
@@ -17,4 +16,10 @@ export class TaskService {
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
   };
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+
+    return this.http.delete<Task>(url);
+  }
 }
